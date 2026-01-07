a06e6b21265477030916cee7a3838ce33f26c80e
Lichen <liu.lichen63@gmail.com>
2026-01-07 17:31:11 +0800

    feat: save window size and user id (#13)

--- lib/models/game_data.dart
+++ lib/models/game_data.dart
@@ -2,11 +2,17 @@ class GameData {
   final int level;
   final double currentExp;
   final bool isAlwaysOnTop;
+  final double? windowWidth;
+  final double? windowHeight;
+  final String? userId;
 
   GameData({
     required this.level,
     required this.currentExp,
     this.isAlwaysOnTop = true,
+    this.windowWidth,
+    this.windowHeight,
+    this.userId,
   });
 
   // Convert a GameData object into a Map object
@@ -15,6 +21,9 @@ class GameData {
       'level': level,
       'currentExp': currentExp.isInfinite ? 0 : currentExp.toInt(),
       'isAlwaysOnTop': isAlwaysOnTop,
+      'windowWidth': windowWidth,
+      'windowHeight': windowHeight,
+      'userId': userId,
     };
   }
 
@@ -24,6 +33,27 @@ class GameData {
       level: json['level'] as int,
       currentExp: (json['currentExp'] as num).toDouble(),
       isAlwaysOnTop: json['isAlwaysOnTop'] as bool? ?? true,
+      windowWidth: (json['windowWidth'] as num?)?.toDouble(),
+      windowHeight: (json['windowHeight'] as num?)?.toDouble(),
+      userId: json['userId'] as String?,
+    );
+  }
+
+  GameData copyWith({
+    int? level,
+    double? currentExp,
+    bool? isAlwaysOnTop,
+    double? windowWidth,
+    double? windowHeight,
+    String? userId,
+  }) {
+    return GameData(
+      level: level ?? this.level,
+      currentExp: currentExp ?? this.currentExp,
+      isAlwaysOnTop: isAlwaysOnTop ?? this.isAlwaysOnTop,
+      windowWidth: windowWidth ?? this.windowWidth,
+      windowHeight: windowHeight ?? this.windowHeight,
+      userId: userId ?? this.userId,
     );
   }
 }
